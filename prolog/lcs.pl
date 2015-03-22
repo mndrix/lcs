dd0693a07fc820bf97e89da8c1345070695adb1f
Michael Hendricks <michael@ndrix.org>
2015-03-21 18:00:24 -0600

    Find regions of a long sequence that are different
    This is a prototype of an idea for making LCS more efficient on long
    sequences with only a few changes between them.
    
    The algorithm is like two people starting at the entrance to a theme
    park.  They both walk the same path together for as long as possible.
    Eventually they part ways.  Now they need to figure out how to
    reconnect.  As they each walk through the park separately, they
    communicate by walkie-talkie.  One says, "I see a trash can with
    Mickey Mouse on it".  The other responds, "I've seen lots of those."
    One says, "I see Pirates of the Caribbean".  The other responds, "I
    haven't seen that."  Eventually one says, "I see Splash Mountain."
    The other responds, "I saw that a minute ago.  Wait there, I'll come
    meet you."  Once they meet up again, the algorithm repeats.
    
    The shared path from the entrance to their point of departure is a
    series of elements shared by both sequences.  The people part ways as
    soon as the sequences differ.  The landmarks are lines (separated by
    \n) within the sequence.  As soon as they both encounter the same
    unique line (seen only once by each side), the one that's "further
    ahead" backtracks.  Now the two sequences are synchronized.  The
    elements that each person passed over before the distinctive landmark
    are pieces of a hunk on which full LCS must be run.  Repeat the
    algorithm to find the next such hunk.
    
    TODO:
    
      * look for the todo/4 predicate.  That needs to be done.
      * left_line//1 gives `` at EOF it should probably fail
      * walk through sync_on_unique_line with an example and see
        what still doesn't behave correctly.

--- prolog/lcs.pl
+++ prolog/lcs.pl
@@ -81,3 +81,166 @@ equality_metric(A,B,Similarity) :-
     ( A==B -> Similarity=1
     ; true -> Similarity=0
     ).
+
+
+% BELOW experimental optimizations to discard identical regions
+
+:- use_module(library(edcg)).
+:- use_module(library(rbtrees)).
+
+edcg:acc_info(left,X,In,Out,In=[X|Out]).
+edcg:acc_info(right,X,In,Out,In=[X|Out]).
+edcg:acc_info(stats,X,In,Out,update_stats(X,In,Out)).
+
+edcg:pred_info(parts,1,[left,right,stats]).
+edcg:pred_info(take_common_line,1,[left,right]).
+edcg:pred_info(take_common_bytes,1,[left,right]).
+edcg:pred_info(clear_rest_of_line,0,[left,right]).
+edcg:pred_info(sync_on_unique_line,2,[left,right,stats]).
+edcg:pred_info(common_line,1,[stats]).
+edcg:pred_info(left_line,1,[left]).
+edcg:pred_info(left_saw_line,2,[stats]).
+edcg:pred_info(swap_left_right,0,[left,right,stats]).
+edcg:pred_info(untangle_swaps,0,[left,right,stats]).
+edcg:pred_info(is_swapped,0,[stats]).
+edcg:pred_info(unique_on_right,2,[stats]).
+
+parts(Left,Right,Parts) :-
+    empty_stats(Stats),
+    parts(Parts,Left,[],Right,[],Stats,_).
+
+parts(Parts) -->>
+    take_common_line(Line),
+    common_line(Line),
+    parts(Parts).
+parts(Parts) -->>
+    take_common_bytes(_Bytes),
+    LeftDeparture/left,
+    RightDeparture/right,
+    clear_rest_of_line,
+    sync_on_unique_line(LeftJoin,RightJoin),
+    todo(Parts,LeftDeparture,RightDeparture,LeftJoin,RightJoin).
+
+empty_stats(stats(straight,Left,Right)) :-
+    rb_empty(Left),
+    rb_empty(Right).
+
+% left and right saw this common line
+common_line(LineCodes) -->>
+    string_codes(Line,LineCodes),
+    stats(Xd,Left0,Right0)/stats,
+    stats/stats(Xd,Left,Right),
+    saw_line(Line,_,Left0,Left),
+    saw_line(Line,_,Right0,Right).
+
+saw_line(LineCodes,Position,Tree0,Tree) :-
+    string_codes(Line,LineCodes),
+    ( rb_lookup(Line,Occur,Tree0) ->
+        ( Occur=multiple -> Tree=Tree0
+        ; Occur=once(_) -> rb_update(Tree0,Line,multiple,Tree)
+        )
+    ; otherwise -> % first occurrence
+        rb_insert(Tree0,Line,once(Position),Tree)
+    ).
+
+% fails if left and right share no leading lines
+take_common_line([C|Line]) -->>
+    [C]:left,
+    [C]:right,
+    C \= 0'\n,
+    !,
+    take_common_line(Line).
+take_common_line([]) -->>
+    C = 0'\n,
+    [C]:left,
+    [C]:right,
+    !.
+take_common_line([]) -->>
+    []/left,
+    []/right.
+
+% unifies argument with leading bytes shared by left and right
+% or [] if left and right share no bytes.
+take_common_bytes([B|Bytes]) -->>
+    [B]:left,
+    [B]:right,
+    !,
+    take_common_bytes(Bytes).
+take_common_bytes([]) -->>
+    true.
+
+
+clear_rest_of_line -->>
+    insert(Left0,Left):left,
+    take_line(_,Left0,Left),
+    insert(Right0,Right):right,
+    take_line(_,Right0,Right).
+
+take_line([],[],[]).
+take_line(Line,[C|Cs0],Cs) :-
+    ( C = 0'\n ->
+        Line=[],
+        Cs=Cs0
+    ; otherwise ->
+        Line=[C|Line1],
+        take_line(Line1,Cs0,Cs)
+    ).
+
+sync_on_unique_line([],[]) -->>
+    []/left,
+    []/right,
+    !.
+sync_on_unique_line(LeftJoin,RightJoin) -->>
+    LeftJoin/left,
+    left_line(Line),
+    unique_on_right(Line,RightJoin),
+    !,
+    right/RightJoin,  % rewind right
+    left/LeftJoin,    % rewind left
+    untangle_swaps.
+sync_on_unique_line(LeftJoin,RightJoin) -->>
+    LeftMark/left,
+    left_line(Line),
+    \+ unique_on_right(Line,_),
+    !,
+    left_saw_line(Line,LeftMark),
+    swap_left_right,
+    sync_on_unique_line(LeftJoin,RightJoin).
+sync_on_unique_line(LeftJoin,RightJoin) -->>
+    \+ left_line(_),
+    swap_left_right,
+    sync_on_unique_line(LeftJoin,RightJoin).
+
+
+left_line(Line) -->>
+    insert(Left0,Left):left,
+    take_line(Line,Left0,Left).
+
+left_saw_line(Line,Position) -->>
+    insert(stats(Xd,Tree0,R),stats(Xd,Tree,R)):stats,
+    saw_line(Line,Position,Tree0,Tree).
+
+swap_left_right -->>
+    insert(Left,Right):left,
+    insert(Right,Left):right,
+    insert(stats(Xd0,L,R),stats(Xd,R,L)):stats,
+    toggle_xd(Xd0,Xd).
+
+toggle_xd(straight,crossed).
+toggle_xd(crossed,straight).
+
+untangle_swaps -->>
+    is_swapped,
+    !,
+    swap_left_right.
+untangle_swaps -->>
+    \+ is_swapped,
+    true.  % no swapping needed
+
+is_swapped -->>
+    stats(swapped,_,_)/stats.
+
+unique_on_right(LineCodes,Position) -->>
+    stats(_,_,R)/stats,
+    string_codes(Line,LineCodes),
+    rb_lookup(Line,once(Position),R).
